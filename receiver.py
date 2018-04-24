dfdb2ae63dd178190aaaefdbd12ed56dc00e32e9
matthowe <mhowe@headingtonschool.com>
2018-04-24 12:01:43 +0100

    Update to include client IP identification
    No idea why from socket import * does not work with gethostbyname
    need to do an import socket - but then socket(AF_INET, SOCK_DGRAM) does not work!
    so doing the extra import.

--- receiver.py
+++ receiver.py
@@ -3,12 +3,19 @@ import os
 from socket import *
 
 
+
+
 host = ""
 port = 13000
 buf = 1024
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 UDPSock.bind(addr)
+
+
+import socket
+print(socket.gethostbyname(socket.gethostname()))
+
 print ("Waiting to receive messages...")
 while True:
     (data, addr) = UDPSock.recvfrom(buf)
